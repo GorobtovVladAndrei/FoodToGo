a4c5c0feb0ca709cc02101538a03c02358227ffe
Gorobtov Vlad Andrei <gorobtovvladandrei@gmail.com>
2024-02-24 17:46:37 +0200

    Modificari auth

---
+++ src/lib/supabase.ts
@@ -0,0 +1,27 @@
+import 'react-native-url-polyfill/auto';
+import * as SecureStore from 'expo-secure-store';
+import { createClient } from '@supabase/supabase-js';
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = 'https://ydycggzqfzguwbsupyaz.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlkeWNnZ3pxZnpndXdic3VweWF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg2ODc3MDYsImV4cCI6MjAyNDI2MzcwNn0.-2uKE7p64ENOXX11zLW2cSRC8VNY0BskYfAYfnhLQ5E';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
